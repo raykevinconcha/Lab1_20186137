102d7562e2504fadda04c15aef86737f58001af1
raykevinconcha <tudela2352>
2023-09-03 21:35:44 -0500

    ganaste /perdiste

--- app/src/main/java/com/example/lab1_20186137/TeleAhorcado.java
+++ app/src/main/java/com/example/lab1_20186137/TeleAhorcado.java
@@ -1,12 +1,15 @@
 package com.example.lab1_20186137;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,6 +23,11 @@ public class TeleAhorcado extends AppCompatActivity {
     private LinearLayout contenedor;
     private Teclado teclado;
     private GridView gridView;
+    private int acierto;
+    private int palabrastotales;
+    private ImageView[]partes;
+    private int intentos=6;
+    private int parte_actual;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         int id_palabras=R.id.palabras;
@@ -33,6 +41,13 @@ public class TeleAhorcado extends AppCompatActivity {
         contenedor=findViewById(id_palabras);
         gridView=findViewById(id_letras);
         random=new Random();
+        partes=new ImageView[intentos];
+        partes[0]=findViewById(R.id.cabeza);
+        partes[1]=findViewById(R.id.torso);
+        partes[2]=findViewById(R.id.manoizquierda);
+        partes[3]=findViewById(R.id.manoderecha);
+        partes[4]=findViewById(R.id.pieizquierdo);
+        partes[5]=findViewById(R.id.piederecho);
         playGame();
 
     }
@@ -52,6 +67,45 @@ public class TeleAhorcado extends AppCompatActivity {
         }
         teclado=new Teclado(this);
         gridView.setAdapter(teclado);
+        acierto=0;
+        parte_actual=0;
+        palabrastotales=a.length();
 
+    }
+    public void clic(View view){
+        String letra=((TextView)view).getText().toString();
+        char letra_t=letra.charAt(0);
+        view.setEnabled(false);
+        boolean correct=false;
+        for(int i=0;i<a.length();i++){
+            if(a.charAt(i)==letra_t){
+                correct=true;
+                acierto++;
+                let[i].setTextColor(Color.BLACK);
+            }
+        }
+        if (correct) {
+            if(acierto==palabrastotales){
+                TextView mensajeGanaste = findViewById(R.id.mensaje_ganaste);
+                mensajeGanaste.setVisibility(View.VISIBLE);
+                disableButtons();
+            }
+        }
+        else if (parte_actual<intentos) {
+            partes[parte_actual].setVisibility(View.VISIBLE);
+            parte_actual++;
+
+
+        }
+        else{
+            disableButtons();
+            TextView mensajePerdiste= findViewById(R.id.mensaje_perdiste);
+            mensajePerdiste.setVisibility(View.VISIBLE);
+        }
+    }
+    public void disableButtons(){
+        for(int i=0;i<gridView.getChildCount();i++){
+            gridView.getChildAt(i).setEnabled(false);
+        }
     }
 }
